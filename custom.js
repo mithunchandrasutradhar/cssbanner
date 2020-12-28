23a4d76a84c7ca83ea8df41919b7d46ca63aa1a6
mithunchandrasutradhar <mithunchandra40@gmail.com>
2020-12-28 12:31:41 +0600

    responsive slider

---
+++ custom.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  $(".Modern-Slider").slick({
+    autoplay: true,
+    autoplaySpeed: 6500,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    pauseOnHover: false,
+    dots: true,
+    cssEase: "ease-out",
+    waitForAnimate: false,
+    fade: true,
+    draggable: false,
+    prevArrow: false,
+    nextArrow: false,
+  });
+});
